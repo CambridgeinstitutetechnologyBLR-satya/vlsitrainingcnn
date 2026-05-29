f0bf71b681b222428fb3088dc479939dcd765778
Satya <satya.24ece@cambridge.edu.in>
2026-05-29 15:01:54 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -21,52 +21,53 @@ module tt_um_1bit_cnn (
             row0 <= 3'b000;
             row1 <= 3'b000;
             row2 <= 3'b000;
-        end
-        else if (data_valid) begin
+        end else if (data_valid) begin
             row0 <= {row0[1:0], pixel_in};
             row1 <= {row1[1:0], row0[2]};
             row2 <= {row2[1:0], row1[2]};
         end
     end
 
-    // XNOR convolution
     wire [8:0] matches;
 
-    assign matches[0] = row0[0] ~^ 1'b0;
-    assign matches[1] = row0[1] ~^ 1'b1;
-    assign matches[2] = row0[2] ~^ 1'b0;
+    assign matches[0] = ~(row0[0] ^ 1'b0);
+    assign matches[1] = ~(row0[1] ^ 1'b1);
+    assign matches[2] = ~(row0[2] ^ 1'b0);
 
-    assign matches[3] = row1[0] ~^ 1'b1;
-    assign matches[4] = row1[1] ~^ 1'b1;
-    assign matches[5] = row1[2] ~^ 1'b1;
+    assign matches[3] = ~(row1[0] ^ 1'b1);
+    assign matches[4] = ~(row1[1] ^ 1'b1);
+    assign matches[5] = ~(row1[2] ^ 1'b1);
 
-    assign matches[6] = row2[0] ~^ 1'b0;
-    assign matches[7] = row2[1] ~^ 1'b1;
-    assign matches[8] = row2[2] ~^ 1'b0;
+    assign matches[6] = ~(row2[0] ^ 1'b0);
+    assign matches[7] = ~(row2[1] ^ 1'b1);
+    assign matches[8] = ~(row2[2] ^ 1'b0);
 
     wire [3:0] match_sum;
 
-    assign match_sum =
-        matches[0] + matches[1] + matches[2] +
-        matches[3] + matches[4] + matches[5] +
-        matches[6] + matches[7] + matches[8];
+    assign match_sum = matches[0] + matches[1] + matches[2] +
+                       matches[3] + matches[4] + matches[5] +
+                       matches[6] + matches[7] + matches[8];
 
     reg conv_output;
 
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
             conv_output <= 1'b0;
+        else if (match_sum >= 4'd5)
+            conv_output <= 1'b1;
         else
-            conv_output <= (match_sum >= 4'd5);
+            conv_output <= 1'b0;
     end
 
-    assign uo_out[0]   = conv_output;
+    assign uo_out[0] = conv_output;
     assign uo_out[7:1] = 7'b0000000;
 
-    assign uio_out = 8'b0;
-    assign uio_oe  = 8'b0;
+    assign uio_out = 8'b00000000;
+    assign uio_oe  = 8'b00000000;
 
-    wire [5:0] unused_inputs = ui_in[7:2];
+    wire [5:0] unused_inputs;
+    assign unused_inputs = ui_in[7:2];
+
     wire _unused;
     assign _unused = &{ena, unused_inputs, uio_in, 1'b0};
 
